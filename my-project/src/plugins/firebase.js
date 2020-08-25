68e54340104ad5a60880c47e673c4b5aefd6206f
ryota <ryota.shunpu.sfz2@gmail.com>
2020-08-25 17:51:15 +0900

    firebase アプリ登録に伴う変更

--- my-project/src/plugins/firebase.js
+++ my-project/src/plugins/firebase.js
@@ -1,13 +1,15 @@
 import * as firebase from 'firebase/app';
 import 'firebase/functions';
 
-const config = {
+const firebaseConfig = {
   apiKey: "AIzaSyAU5o8Y_tCYtixaUX_YqhoK4_-hlmshHmY",
   authDomain: "myprotfolio-project.firebaseapp.com",
   databaseURL: "https://myprotfolio-project.firebaseio.com",
   projectId: "myprotfolio-project",
   storageBucket: "myprotfolio-project.appspot.com",
-  messagingSenderId: "354664926376"
+  messagingSenderId: "354664926376",
+  appId: "1:354664926376:web:846659ae8defa6c8cc069d",
+  measurementId: "G-E0PHC2EZYK"
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 export const functions = firebase.functions();
